739db570a976667f74114b13073db5fd04cd74a7
OneMillionthUsername <dean.marinov@gmx.at>
2024-10-29 08:33:12 +0100

    clean up some commentory. postponed

--- StartServer_pure_ded.bat
+++ StartServer_pure_ded.bat
@@ -4,5 +4,5 @@ set Q4ModName=automod
 set Q4InstallPath=C:\Program Files (x86)\Steam\steamapps\common\Quake 4
 set CWD=%cd%
 cd %Q4InstallPath%
-start "" "%Q4InstallPath%\Quake4.exe" +set net_serverDedicated 1 +set com_allowConsole 1 +set logfile 2 +set fs_game %Q4ModName% +set si_pure 1 +set developer 1 +set win_allowmultipleInstances 1 +set fs_savepath "%Q4InstallPath%\host\" +set si_map mp/q4dm1 +spawnserver
+start "" "%Q4InstallPath%\Quake4.exe" +set net_serverDedicated 1 +set com_allowConsole 1 +set logfile 2 +set fs_game %Q4ModName% +set si_pure 1 +set developer 1 +set win_allowmultipleInstances 1 +set fs_savepath "%Q4InstallPath%\host\" +set si_map mp/q4dm1 +exec server.cfg +spawnserver
 cd %CWD%
